bf751885b0c37ec9476e0eedfc53f269a9bc0fa5
billy <65816192+bilbiall@users.noreply.github.com>
2023-03-20 00:41:18 +0300

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,12 @@
+
+function showForm() {
+var form = document.getElementById("purchase-form");
+form.style.display = "block";
+}
+
+function hideForm() {
+    var form = document.getElementById("purchase-form");
+    form.style.display = "none";
+  }
+
+  
